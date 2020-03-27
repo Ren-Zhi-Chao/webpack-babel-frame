c036ecb0fbaa93355bc6289d4e044c5955ccaee6
zhichao.ren <973063956@qq.com>
2020-03-27 11:59:20 +0800

    *) 修改工程原本依赖

--- builder/entry.build.js
+++ builder/entry.build.js
@@ -1,10 +1,50 @@
 import Handlebars from 'handlebars';
 import _path from 'path';
 import fs from 'fs';
-import FileUtils from '../src/utils/file.util';
-import StringUtils from '../src/utils/string.util'
 const domain = require('../package.json');
 const index = _path.resolve(__dirname, '../', domain.entry);
+const _join = _path.join;
+
+/*************************工具集*************************/
+/**
+ * @param startPath  起始目录文件夹路径
+ * @param relative 相对的路径
+ * @returns {Array}
+ */
+function findSync(startPath, relative) {
+    let result = [];
+    function finder(path) {
+        const files = fs.readdirSync(path);
+        files.forEach((val) => {
+            let fPath = _join(path, val);
+            let stats = fs.statSync(fPath);
+            if(stats.isDirectory()) finder(fPath);
+            if(stats.isFile()) result.push(fPath);
+        });
+    }
+    finder(startPath);
+    if (relative) result = result.map(row => _path.relative(relative, row))
+    return result;
+}
+
+// 下划线转换驼峰
+function toHump(name) {
+    return name.replace(/\_(\w)/g, function(all, letter){
+        return letter.toUpperCase();
+    });
+}
+
+// 驼峰转换下划线
+function toLine(name) {
+  return name.replace(/([A-Z])/g,"_$1").toLowerCase();
+}
+
+// 首字母大写
+function firstToUpCase(str) {
+    str = str ? str : '';
+    return str.replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+}
+/*************************工具集*************************/
 
 const content = 
 `{{ imports }}
@@ -25,19 +65,22 @@ const rowImport =
 `import {{filename}} from '{{filepath}}';
 `
 
+const coreIgnore = [ 'README.md' ];
+
 function getContent() {
     const template = Handlebars.compile(content, { noEscape: true });
     const importTemp = Handlebars.compile(rowImport, { noEscape: true });
     const core = _path.resolve(__dirname, '../', 'src', 'core');
-    const importsStr = FileUtils.findSync(core, _path.resolve(index, '../')).map(row => row.replace(/.js/g, ''));
+    const importsStr = findSync(core, _path.resolve(index, '../')).map(row => row.replace(/.js/g, ''));
     
     let imports = '';
     const exports = [];
-    importsStr.forEach(filepath => {
-        const filename = StringUtils.firstToUpCase(StringUtils.toHump(_path.basename(filepath)));
-        exports.push(filename);
-        imports += importTemp({ filename, filepath: './' + filepath });
-    })
+    importsStr.filter(row => !coreIgnore.includes(_path.basename(row)))
+        .forEach(filepath => {
+            const filename = firstToUpCase(toHump(_path.basename(filepath)));
+            exports.push(filename);
+            imports += importTemp({ filename, filepath: './' + filepath });
+        })
     return template({ imports, exports: exports.join(', ') })
 }
 
